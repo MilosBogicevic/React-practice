8941196dbada5bc87f2dcf4486cfa2051902fe2c
MilosBogicevic <milbog.development@gmail.com>
2020-06-28 20:34:05 +0200

    Added avatar props from index.js to CommentForm.js.

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,11 +1,10 @@
 import React from "react";
-import faker from "faker";
 
 const CommentForm = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
@@ -14,7 +13,7 @@ const CommentForm = (props) => {
         <div className="metadata">
           <span className="date">{props.date}</span>
         </div>
-        <div className="text">{props.text}</div>
+        <div className="text">{props.content}</div>
       </div>
       <hr />
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentForm from "./components/CommentForm";
+import faker from "faker";
 
 const App = () => {
   return (
@@ -9,27 +10,32 @@ const App = () => {
       <CommentForm
         author="Marko Petrovic"
         date="5 minutes ago"
-        text="This course is amazing."
+        content="This course is amazing."
+        avatar={faker.image.avatar()}
       />
       <CommentForm
         author="Nenad Marinkovic"
         date="1 day ago"
-        text="The course is what a newbie like me could understand."
+        content="The course is what a newbie like me could understand."
+        avatar={faker.image.avatar()}
       />
       <CommentForm
         author="Petar Zivkovic"
         date="1 day ago"
-        text="This course is very useful for beginners."
+        content="This course is very useful for beginners."
+        avatar={faker.image.avatar()}
       />
       <CommentForm
         author="Ivan Novakovic"
         date="3 days ago"
-        text="Short, to the point and very easy to follow."
+        content="Short, to the point and very easy to follow."
+        avatar={faker.image.avatar()}
       />
       <CommentForm
         author="Marko Ivanovic"
         date="4 days ago"
-        text="This tutorial proved to be very helpful for me."
+        content="This tutorial proved to be very helpful for me."
+        avatar={faker.image.avatar()}
       />
     </div>
   );
